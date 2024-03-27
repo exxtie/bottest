11622ec2bead2ada97bb7392973837f96e2e8624
Egor Basnak <basnak2014@mail.ru>
2024-03-27 16:40:45 +0300

    First Echo-Bot

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-def ttt(number: int, word: str) -> str:
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
-    return word
+BOT_TOKEN = '6888044785:AAFtRgFd9wSTZd39s_NBPBkfnWmKeXeI_X0'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
 
-def main():
-    return 0
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=str(message.text))
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
